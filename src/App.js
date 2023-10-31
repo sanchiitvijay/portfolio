80dc6a08dedbf20e65ee176b16ea004245b847f2
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2023-10-31 09:26:00 +0530

    31-10-23-i

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/header/Header.jsx';
 import Home from './components/home/Home.jsx';
 import About from './components/about/About.jsx';
+import Skills from './components/skills/Skills.jsx';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main className='main'>
         <Home />
         <About />
+        <Skills />
       </main>
     </div>
   );
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Skills (){
+    return(
+        <section className='skills section' id='skills'>
+            <h2 className='section_title'>Skills</h2>
+            <span className='section_subtitle'>My skills</span>
+        
+            <div className='skills_container container grid'>
+                
+            </div>
+        </section>
+    )
+}
+export default Skills;
